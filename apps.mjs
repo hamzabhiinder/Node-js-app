94e6733fb21f9993c7d2091365cc427850d4a729
pc <amoizj16@gmail.com>
2022-09-26 20:01:09 +0500

    Todo App

--- apps.mjs
+++ apps.mjs
@@ -1,13 +1,35 @@
 import express from 'express'
+import cors from "cors";
 const app = express()
 const port = process.env.port ||3000
 
-app.get('/', (req, res) => {
-    console.log('Some request are action',req.ip)
-    console.log('changes')
-  res.send('Hello my name is Ameer Hamza Here COmputer')
+app.use(express.json())
+app.use(cors())
+
+
+let todos=[]
+
+
+app.get('/del', (req, res) => {
+ 
+  todos=[]
+})
+app.get('/todos', (req, res) => {
+ 
+  res.send({
+    messages:"Your data Message ",
+    data: todos
+  })
+})
+app.post('/todo', (req, res) => {
+  todos.push(req.body.text)  
+  res.send({
+    messages:"Your Message has Send",
+    data: todos
+  })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
